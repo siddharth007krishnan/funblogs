58975c8d7b138724e7abe71480c123c9422af45a
Siddharth Krishnan <immadridista007@outlook.com>
2018-03-03 00:02:37 +0530

    Added Changes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/blogs';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/blogs';
 
     /**
      * Create a new controller instance.
